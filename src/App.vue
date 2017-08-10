84a1969d22b48d462934628b6744e0548b000c35
victor <victor.efetivos@gmail.com>
2017-08-10 10:42:50 -0300

    Insert All Images & Page Contato

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view></router-view>
   </div>
 </template>
@@ -11,13 +10,4 @@ export default {
 }
 </script>
 
-<style>
-#app {
-    font-size: 50px;
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-</style>
+<style src="./components/css/css_app.css"></style>
--- src/components/Contato.vue
+++ src/components/Contato.vue
@@ -1,5 +1,44 @@
 <template>
-<router-link to="/"><div id="forca"> Olaa  Treta</div></router-link>
+
+<body>
+
+<main id="body">
+
+<!-- Init Contato -->
+<div class="title"><h1 id="titleContato">Contato</h1></div>
+<div class="photoBg"></div>
+
+<!-- Container Infos -->
+<section class="containerInfos">
+
+<!-- Box Chegous -->
+<div class="boxContato" id="boxChegou">
+  <h1 id="chegou">Chegou a hora de apertar as mãos:</h1>
+  <p id="boraTomar">Bora tomar um cafézinho, comer um croissant e conversar sobre seu projeto:</p>
+</div>
+
+<!-- Box Contato -->
+<div class="boxContato" id="boxMsg">
+<div class="btnEnviar"><p id="enviarMsg">ENVIAR MENSAGEM</p></div>
+	<p id="contatos"><span>T.</span> +55 (19) 9 81349.1395<br><span>E.</span> Contato@efetivos.com.br</p><br>
+	<div id="boxCont"><p id="contatoDesk1"><span>T.</span>+55 (19) 9 81349.1395</p><p id="contatoDesk2"><span>E.</span>Contato@efetivos.com.br</p></div>
+
+	<div id="containerIcons">
+		<div id="facebook"></div>
+		<div id="vimeo"></div>
+		<div id="instagram"></div>
+		<div id="whatsapp"></div>			
+	</div>
+</div>
+</section>
+
+<!-- Box Rodape -->
+<div id="rodape">
+	<div id="logoFlat"></div>
+</div>
+</main>
+
+</body>
 </template>
 
 
@@ -8,4 +47,4 @@
 </script>
 
 
-<style src="./css/style.css"></style>
+<style src="./css/style_contato.css"></style>
