33e5dc86f1af9d703ada83355047f61f10dd7ced
victor <victor.efetivos@gmail.com>
2017-08-11 15:52:28 -0300

    TweenMax & Timeline Funcionando

--- src/components/GsapTest.vue
+++ src/components/GsapTest.vue
@@ -1,22 +1,30 @@
 <template>
-    <div id="box">    
+<div>
+    <div id="box"></div>
+    <div id="box2"></div>
     </div>
 </template>
 
 <script>
-import { TweenMax } from 'gsap'
+import { TweenMax, TimelineMax } from 'gsap'
+// import { TimelineMax } from 'gsap'
 
-export default { mounted () { TweenMax.from('#box', 5, {width: 0}) }
+export default { mounted () { TweenMax.from('#box', 5, {width: 0}); const tlVueGsap = new TimelineMax(); tlVueGsap.from('#box2', 5, {width: 0}).to('#box', 5, {opacity: 0}) }
 }
 </script>
 
 <style>
 #box{
-    position: absolute;
     margin: 50px 50px;
     width: 80px;
     height: 80px;
     background-color: red;
+}
+#box2{
+    margin: 50px 50px;
+    width: 80px;
+    height: 80px;
+    background-color: blue;
 }
 
 </style>
