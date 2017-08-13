e48a6fa2080238fe69e8d69681339927c67748c2
victor <victor.efetivos@gmail.com>
2017-08-12 21:43:40 -0300

    Fix Error Path Alias

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -2,10 +2,6 @@ var path = require('path')
 var utils = require('./utils')
 var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
-var $ = require("jquery");
-var ScrollMagic = require("scrollmagic");
-require('scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap');
-var gsap = require("gsap");
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -26,14 +22,7 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
-      "TweenLite": Path.resolve('node_modules', 'gsap/src/uncompressed/TweenLite.js'),
-      "TweenMax": Path.resolve('node_modules', 'gsap/src/uncompressed/TweenMax.js'),
-      "TimelineLite": Path.resolve('node_modules', 'gsap/src/uncompressed/TimelineLite.js'),
-      "TimelineMax": Path.resolve('node_modules', 'gsap/src/uncompressed/TimelineMax.js'),
-      "ScrollMagic": Path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/ScrollMagic.js'),
-      "animation.gsap": Path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js'),
-      "debug.addIndicators": Path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js')
+      '@': resolve('src')
     }
   },
   module: {
