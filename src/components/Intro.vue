2394f78f0953d48419c5495e9cd20070b7cd160c
victor <victor.efetivos@gmail.com>
2018-04-29 17:23:16 -0300

    Tl Working

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,7 +1,11 @@
 <template>
 <div>
     <main class="ctn-main e-wvw e-hvh e-rel e-flex-col">
+        
+        <!-- --------- photo-holder --------- -->
+        <img src="./images/intro/eyes1.jpg" alt="" class="slide-intro e-wvw e-img-fit e-hvh mobile-intro">
 
+        
         <!-- --------- holder apenas seja --------- -->
         <div class="box-apenas e-flex e-fix">
             <div class="holder-tracos e-wp e-hp e-rel">
@@ -34,11 +38,10 @@
             </div>
         </div>
 
-
-
-
     </main>
 
+    <div class="reveal reveal-up e-wvw"></div>
+    <div class="reveal reveal-down e-wvw"></div>
 </div>
 </template>
 
--- src/components/js/intro.js
+++ src/components/js/intro.js
@@ -2,6 +2,11 @@
 import { TweenMax } from 'gsap'
 import $ from 'jquery'
 import SplitText from 'gsap/SplitText'
+import img1 from '../images/intro/eyes1.jpg'
+import img2 from '../images/intro/eyes2.png'
+import img3 from '../images/intro/eyes3.png'
+import img4 from '../images/intro/eyes4.png'
+import img5 from '../images/intro/eyes5.png'
 
 
 
@@ -14,9 +19,22 @@ export default {
 
     mounted() {
         TweenMax.set('html', { overflow: 'hidden !important' })
-        var durations = 3
+        var durations = 3,
+        $revealUp = $('.reveal-up'),
+        $revealDown = $('.reveal-down'),
+        slideIntro = $('.slide-intro')
 
         TweenLite.defaultEase = Linear.easeNone;
+
+
+        TweenMax.set('.ctn-menu',{opacity:0})
+        let tlIntro = new TimelineMax({repeat:-1}).timeScale(1.4)
+          .set(slideIntro,{attr:{ src:''+img2}},'+=.2')
+          .set(slideIntro,{attr:{ src:''+img3}},'+=.2')
+          .set(slideIntro,{attr:{ src:''+img4}},'+=.2')
+          .set(slideIntro,{attr:{ src:''+img1}},'+=.2')
+          .set(slideIntro,{attr:{ src:''+img1}},'+=.2')
+
 
 
 
@@ -31,16 +49,30 @@ export default {
                 .duration(durations)
         }
 
+
+        
         //<!-- --------- tlLets Seja --------- -->
         var myLets = new SplitText(".eftv-lets", { type: "chars, words" }),
             tlLets = new TimelineMax(),
             numLets = myLets.chars.length
             
         for (var i = 0; i < numLets; i++) {
-            //random value used as position parameter
             tlLets.from(myLets.chars[i], 2, { opacity: 0 }, Math.random() * 10)
                 .add('end')
-                .duration(durations)
+                .duration(8)
+        }
+
+
+
+        //<!-- --------- tlTitle  --------- -->
+        var myTitle = new SplitText(".title-efetivos", { type: "chars, words" }),
+            tlTitle = new TimelineMax(),
+            numTitle = myTitle.chars.length
+            
+        for (var i = 0; i < numTitle; i++) {
+            tlTitle.from(myTitle.chars[i], 2, { opacity: 0 }, Math.random() * 10)
+                .add('end')
+                .duration(3)
         }
 
 
@@ -58,11 +90,31 @@ export default {
         //tlLines.tweenTo('end')
 
 
+        //<!-- --------- REVEAL --------- -->
+        var tlReveal = new TimelineMax()
+                .to($revealUp, 2.56, {scaleY:0, transformOrigin:'right top', ease: Power1.easeIn},0)
+                .to($revealDown, 2.56, {scaleY:0, transformOrigin:'right bottom', ease: Power1.easeIn},0)
+                .duration(durations)
+
+
+        //<!-- --------- scale destaque --------- -->
+        var tlScale = new TimelineMax()
+            .from('.slide-intro', 3, {scale:1.2, rotation:20, ease: Power3.easeOut});
 
 
 
 
 
+        var tlMaster = new TimelineMax()
+            .add(tlReveal)
+            .add(tlIntro,'0')
+            .add(tlLines,'0')
+            .add(function(){ tlIntro.pause()},'3')
+            .to('.box-apenas',.6,{opacity:0})
+
+            .add(tlTitle,'3.5')
+            .add(tlLets,'3.5')
+            .add(tlScale,'3.5')
 
 
 
